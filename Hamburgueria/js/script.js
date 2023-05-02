b4894ffcb65810cf55fcee9ed3831b2742977626
HendrickPhillip <105526467+HendrickPhillip@users.noreply.github.com>
2023-05-02 16:56:34 -0300

    Add files via upload

---
+++ Hamburgueria/js/script.js
@@ -0,0 +1,16 @@
+class Produtos{
+    constructor(){
+        this.peso = 0;
+        this.nome = '';
+        this.valor = 0;
+    }
+    adicionar(){
+        alert("Adicionado");
+        
+    }
+    excluir(){
+        alert("Excluido");
+    }
+}
+
+var produtos = new Produtos();
