e341528a54886377e484ccd563564bd020805a4e
HendrickPhillip <105526467+HendrickPhillip@users.noreply.github.com>
2023-05-19 14:29:33 -0300

    Add files via upload

---
+++ Trablo Clovis/clovis/src/main/java/com/clovis/Facade/Farois.java
@@ -0,0 +1,11 @@
+package com.clovis.Facade;
+
+public class Farois {
+    public void ligandoFarois(int position, String info) {
+        System.out.println("Ligando farois");
+    }
+
+    public void prontoFarois(int position, String info) {
+        System.out.println("Farois ligados");
+    }
+}
