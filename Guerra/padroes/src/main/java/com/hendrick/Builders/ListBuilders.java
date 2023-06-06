1d1f0bb1d3464242b5f4aaec45a77192ce93455a
HendrickPhillip <hendrick.phillip@aluno.uniaeso.edu.br>
2023-06-05 22:39:38 -0300

    update
    Singleton

--- Guerra/padroes/src/main/java/com/hendrick/Builders/ListBuilders.java
+++ Guerra/padroes/src/main/java/com/hendrick/Builders/ListBuilders.java
@@ -7,38 +7,66 @@ public class ListBuilders {
     TerrestreBuilder terrestreBuilder;
 
     public void montSold(SoldadoBuilder sBuilder) {
-        soldadoBuilder = sBuilder;
-        soldadoBuilder.uniformeCombate();
-        soldadoBuilder.armasFogo();
-        soldadoBuilder.equipComunicação();
-        soldadoBuilder.mochilasTransporte();
-        soldadoBuilder.equipTaticos();
-        soldadoBuilder.ferramentasCombate();
+        // Aqui é onde o padrão Singleton é aplicado
+        if (navalBuilder == null) {
+            System.out.println("Instância criada.");
+            soldadoBuilder = sBuilder;
+            soldadoBuilder.uniformeCombate();
+            soldadoBuilder.armasFogo();
+            soldadoBuilder.equipComunicação();
+            soldadoBuilder.mochilasTransporte();
+            soldadoBuilder.equipTaticos();
+            soldadoBuilder.ferramentasCombate();
+        } else {
+            System.out.println("Instância já existente.");
+        }
+
     }
 
     public void montNaval(NavalBuilder nBuilder) {
-        navalBuilder = nBuilder;
-        navalBuilder.armasFogo();
-        navalBuilder.misseisDefesa();
-        navalBuilder.defesaAntiaerea();
-        navalBuilder.sistemaSonar();
+        // Aqui é onde o padrão Singleton é aplicado
+        if (navalBuilder == null) {
+            System.out.println("Instância criada.");
+            navalBuilder = nBuilder;
+            navalBuilder.armasFogo();
+            navalBuilder.misseisDefesa();
+            navalBuilder.defesaAntiaerea();
+            navalBuilder.sistemaSonar();
+        } else {
+            System.out.println("Instância já existente.");
+        }
 
     }
 
     public void montAreo(AereoBuilder aBuilder) {
-        aereoBuilder = aBuilder;
-        aereoBuilder.armasFogo();
-        aereoBuilder.bombas();
-        aereoBuilder.misseisArAr();
-        aereoBuilder.tanquesCombustívelExternos();
+        // Aqui é onde o padrão Singleton é aplicado
+        if (navalBuilder == null) {
+            System.out.println("Instância criada.");
+            aereoBuilder = aBuilder;
+            aereoBuilder.armasFogo();
+            aereoBuilder.bombas();
+            aereoBuilder.misseisArAr();
+            aereoBuilder.tanquesCombustívelExternos();
+        } else {
+            System.out.println("Instância já existente.");
+        }
+
     }
 
     public void montTank(TerrestreBuilder tBuilder) {
-        terrestreBuilder = tBuilder;
-        terrestreBuilder.blindagem();
-        terrestreBuilder.canhaoPrincipal();
-        terrestreBuilder.metralhadoras();
-        terrestreBuilder.misseis();
+        // Aqui é onde o padrão Singleton é aplicado
+        if (navalBuilder == null) {
+            System.out.println("Instância criada.");
+            terrestreBuilder = tBuilder;
+            terrestreBuilder.blindagem();
+            terrestreBuilder.canhaoPrincipal();
+            terrestreBuilder.metralhadoras();
+            terrestreBuilder.misseis();
+        } else {
+            System.out.println("Instância já existente.");
+        }
+
     }
+
 
 }
